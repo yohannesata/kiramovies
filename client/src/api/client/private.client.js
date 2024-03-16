1f4c7f6a2629123f40b0ba91d36b0d1ba2195c08
yohannesata <111045012+yohannesata@users.noreply.github.com>
2024-03-17 00:54:36 +0300

    Update private.client.js

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://kiramoviesapi.vercel.app/api/v1/";
+const baseURL = "https://kiramoviesapi.vercel.app";
 
 const privateClient = axios.create({
   baseURL,
