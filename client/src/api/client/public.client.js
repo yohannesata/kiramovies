c4a2e152a983fa0657ae6a30e702a072b57dcd20
yohannesata <111045012+yohannesata@users.noreply.github.com>
2024-03-17 01:24:32 +0300

    Update public.client.js

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://kiramovies.vercel.app";
+const baseURL = "https://moonflix-api.vercel.app/api/v1/";
 
 const publicClient = axios.create({
   baseURL,
